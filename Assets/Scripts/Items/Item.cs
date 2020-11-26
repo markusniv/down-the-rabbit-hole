129f5fc29aa1cf1eeaf8ff4ab5e874d22f62c6a9
Markus Nivasalo <markus.nivasalo@outlook.com>
2020-11-26 13:40:18 +0200

    Initial commit

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class Item : MonoBehaviour
+{
+    public int Value;
+
+    [SerializeField] private Sprite _icon = null;
+    public Sprite Icon => _icon;
+
+    #region Components
+    public SpriteRenderer SpriteRenderer { get; set; }
+    #endregion
+
+    public virtual string Tooltip { get; }
+
+    public Inventory Inventory { get; set; }
+
+    private GameObject RecentlyDroppedBy;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Items/Weapons/Weapon.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item
+{
+
+    public override string Tooltip => string.Format("{0}\n" +
+                                                        "Damage: {1}\n" +
+                                                        "Attack speed: {2}\n" +
+                                                        "Cooldown: {3} seconds", weaponName, damage, rotationSpeed, attackCooldownDefault);
+    public float rotationSpeed,
+                 stabSpeed,
+                 damage,
+                 attackCooldownDefault,
+                 attackCooldown;
+
+    public Character character;
+    public CharacterCombat CharacterCombat;
+    public Transform hand;
+
+    private RollWeaponType weaponType;
+
+    public string weaponName;
+
+    private SpriteRenderer sr;
+    private TrailRenderer tr;
+    private Collider2D col;
+
+    public bool attack,
+                attackStarted;
+
+    public Sprite weaponSprite,
+                  weaponSpriteHeld;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
