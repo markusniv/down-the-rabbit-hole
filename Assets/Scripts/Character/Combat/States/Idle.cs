49164af1353ef713ce5b31085bd1476160b304bb
Joonas Kajava <me@joonaskajava.com>
2020-11-30 16:31:37 +0200

    added rest of the states

---
+++ Assets/Scripts/Character/Combat/States/Idle.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// Idle state. Characters in this state can perform attack actions.
+/// </summary>
+public class Idle : State
+{
+    public Idle(Character character) : base(character)
+    {
+    }
+}
---
+++ Assets/Scripts/Character/Movement/States/FollowTarget.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+/// <summary>
+/// Characters in this state will follow specified character.
+/// </summary>
+public class FollowTarget : State
+{
+    public Character Target { get; set; }
+
+    public float FollowDistance = 2f;
+
+    public bool AtRange { get; private set; }
+
+    CircleCollider2D CharacterCollider;
+    CircleCollider2D TargetCollider;
+
+    public FollowTarget(Character character, Character target) : base(character)
+    {
+        Target = target;
+        CharacterCollider = character.GetComponent<CircleCollider2D>();
+        TargetCollider = character.GetComponent<CircleCollider2D>();
+    }
+
+    public override void OnFixedUpdate()
+    {
+        base.OnFixedUpdate();
+        if(DistanceToTarget > FollowDistance)
+        {
+            AtRange = false;
+            Character.Movement.Movement = DirectionToTarget;
+        }else
+        {
+            AtRange = true;
+            Character.Movement.Movement = Vector2.zero;
+        }
+    }
+
+    private Vector3 DirectionToTarget => (TargetCollider.bounds.center - CharacterCollider.bounds.center).normalized;
+    private float DistanceToTarget => Vector3.Distance(TargetCollider.bounds.center, CharacterCollider.bounds.center);
+}
