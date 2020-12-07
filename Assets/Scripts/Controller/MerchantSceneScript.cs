24f95bf2509d0d496670b668d918fd29a2841b8c
Markus Nivasalo <markus.nivasalo@outlook.com>
2020-12-07 10:15:12 +0200

    Begun creating merchant room

---
+++ Assets/Scripts/Controller/MerchantSceneScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MerchantSceneScript : MonoBehaviour
+{
+    Text priceOne, priceTwo, priceThree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        priceOne = GameObject.Find("PriceOne").GetComponent<Text>();
+        priceTwo = GameObject.Find("PriceTwo").GetComponent<Text>();
+        priceThree = GameObject.Find("PriceThree").GetComponent<Text>();
+
+        var possibleConsumables = Resources.LoadAll<GameObject>("Prefabs/Items/Consumables");
+        //var possibleRelics = Resources.LoadAll<GameObject>("Prefabs/Items/Relics");
+        var possibleWeapons = Resources.LoadAll<GameObject>("Prefabs/Items/Weapons");
+        var selectedItemOne = possibleConsumables[Random.Range(0, possibleConsumables.Length)];
+        //var selectedItemTwo = possibleRelics[Random.Range(0, possibleRelics.Length)];
+        var selectedItemThree = possibleWeapons[Random.Range(0, possibleWeapons.Length)];
+
+        var itemOne = Instantiate(selectedItemOne, new Vector3(-7, -2, 0), Quaternion.identity);
+        //var itemTwo = Instantiate(selectedItemTwo, new Vector3(0, -2, 0), Quaternion.identity);
+        var itemThree = Instantiate(selectedItemThree, new Vector3(7, -2, 0), Quaternion.identity);
+
+        itemOne.name = itemOne.name.Replace("(Clone)", "");
+        //itemTwo.name = itemTwo.name.Replace("(Clone)", "");
+        itemThree.name = itemThree.name.Replace("(Clone)", "");
+
+        priceOne.text = itemOne.GetComponent<Item>().Value.ToString();
+        //priceTwo.text = itemTwo.GetComponent<Item>().Value.ToString();
+        priceThree.text = itemThree.GetComponent<Item>().Value.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/SetLetters.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetLetters : MonoBehaviour
+{
+    Text _dialog;
+    string dialogText;
+    char[] dialogArray;
+
+    int counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _dialog = GetComponent<Text>();
+        dialogText = "Buy something, will ya?";
+        dialogArray = dialogText.ToCharArray();
+
+        counter = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (counter < dialogText.Length)
+        {
+            AddLetter(counter);
+            counter++;
+        }
+    }
+
+    void AddLetter(int index)
+    {
+        _dialog.text += dialogArray[index];
+    }
+}
